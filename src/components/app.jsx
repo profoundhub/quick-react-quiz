90055629e02505480b741230a2ad1f53b4fa3ddf
Daniel Lim <hbt@markhamboards.com>
2016-09-02 20:36:49 -0400

    questions functions
    questions functions

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import QuestionList from "./quiz/QuestionList.jsx";
 
 class App extends Component {
   constructor(props) {
@@ -130,7 +131,7 @@ class App extends Component {
   render() {
     return (
       <div>
-          questions
+          <QuestionList {...this.state} />
       </div>
     )
   }
--- src/components/quiz/QuestionList.jsx
+++ src/components/quiz/QuestionList.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import Question from "./Question.jsx";
 
 class QuestionList extends Component {
-
-  }
-
   render() {
     return (
       <div className="questions">
           {
-
+            this.props.questions.map(question => {
+              return
+                <Question question={ question }
+                          key={ question.id } {
+                            ...this.props
+                          }
+                />
+            })
           }
       </div>
     )
