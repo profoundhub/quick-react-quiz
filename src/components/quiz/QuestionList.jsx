cdd2bb50263a4eb100b93d75a7a969567b18b843
Daniel Lim <hbt@markhamboards.com>
2016-09-02 20:24:26 -0400

    setup Question List JSX

---
+++ src/components/quiz/QuestionList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Question from "./Question.jsx";
+
+class QuestionList extends Component {
+
+  }
+
+  render() {
+    return (
+      <div className="questions">
+          {
+
+          }
+      </div>
+    )
+  }
+}
+
+export default QuestionList
